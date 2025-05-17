c0cbf1d74c66c19f4107696a714a1712548caa67
Julia Ilasova <1julka1il@gmail.com>
2025-05-17 15:12:38 +0200

    use better matching in tests

--- GroupMicroservice.Tests/UserApiTests.cs
+++ GroupMicroservice.Tests/UserApiTests.cs
@@ -4,11 +4,11 @@ using System.Text.Json;
 using FluentAssertions;
 using GroupMicroservice.Application;
 using GroupMicroservice.Domain.DTOs;
-using GroupMicroservice.Domain.Exceptions;
 using Moq;
 using PactNet;
 using PactNet.Output.Xunit;
 using Xunit.Abstractions;
+using Match = PactNet.Matchers.Match;
 
 namespace GroupMicroservice.Tests;
 
@@ -53,10 +53,10 @@ public class UserApiTests
             .WithStatus(HttpStatusCode.OK)
             .WithJsonBody(new
                 {
-                    id,
-                    email = "string",
-                    nickname = "string",
-                    createdAt = "2009-07-27T00:00:00"
+                    Id = Match.Regex("03247A5F-B7C9-4B88-8A6B-A3B583594EFD", @"^[{(]?[0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12}[)}]?$"),
+                    Email = Match.Type("string"),
+                    Nickname = Match.Type("string"),
+                    CreatedAt = Match.Regex("2009-07-27T00:00:00", @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
                 }
             );
 
@@ -125,32 +125,34 @@ public class UserApiTests
             Guid.Parse("A1B2C3D4-E5F6-7890-1234-56789ABCDEF0")
         };
 
-        var expectedUsers = new List<object>
-        {
-            new
-            {
-                id = userIds[0],
-                email = "user1@example.com",
-                nickname = "User1",
-                createdAt = "2023-01-01T00:00:00"
-            },
-            new
-            {
-                id = userIds[1],
-                email = "user2@example.com",
-                nickname = "User2",
-                createdAt = "2023-01-02T00:00:00"
-            }
-        };
-
         // Arrange
         _pact.UponReceiving("A valid batch request for users by IDs")
+            .Given("users with the specified IDs exist", new Dictionary<string, string>
+            {
+                ["ids"] = string.Join(",", userIds)
+            })
             .WithRequest(HttpMethod.Post, "/api/v1/user/batch")
             .WithHeader("Content-Type", "application/json")
             .WithJsonBody(userIds)
             .WillRespond()
             .WithStatus(HttpStatusCode.OK)
-            .WithJsonBody(expectedUsers);
+            .WithJsonBody(Match.Type(new[]
+            {
+                new
+                {
+                    Id = Match.Regex("03247A5F-B7C9-4B88-8A6B-A3B583594EFD", @"^[{(]?[0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12}[)}]?$"),
+                    Email = Match.Type("string"),
+                    Nickname = Match.Type("string"),
+                    CreatedAt = Match.Regex("2009-07-27T00:00:00", @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
+                },
+                new
+                {
+                    Id = Match.Regex("A1B2C3D4-E5F6-7890-1234-56789ABCDEF0", @"^[{(]?[0-9A-Fa-f]{8}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{4}[-]?[0-9A-Fa-f]{12}[)}]?$"),
+                    Email = Match.Type("string"),
+                    Nickname = Match.Type("string"),
+                    CreatedAt = Match.Regex("2009-07-27T00:00:00", @"\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}")
+                }
+            }));
 
         await _pact.VerifyAsync(async ctx =>
         {
@@ -186,6 +188,10 @@ public class UserApiTests
 
         // Arrange
         _pact.UponReceiving("A request for users by IDs that do not exist")
+            .Given("users with the specified IDs do not exist", new Dictionary<string, string>
+            {
+                ["ids"] = string.Join(",", userIds)
+            })
             .WithRequest(HttpMethod.Post, "/api/v1/user/batch")
             .WithHeader("Content-Type", "application/json")
             .WithJsonBody(userIds)
