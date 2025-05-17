348cce18c11037ac8ddbad8f33b192f2e0ac66f6
Julia Ilasova <1julka1il@gmail.com>
2025-05-17 10:01:23 +0200

    oops we need to do it in the other service

--- UserMicroservice/Domain/DTOs/GetUserDto.cs
+++ UserMicroservice/Domain/DTOs/GetUserDto.cs
@@ -5,6 +5,6 @@ public class GetUserDto
     public required Guid Id { get; set; }
     public required string Nickname { get; set; } 
     public required string Email { get; set; }
-    public required DateTime CreatedAt { get; set; }
+    // public required DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
